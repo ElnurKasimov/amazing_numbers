ae2cf5a8f85446ffc8c7e3e7d7a04e846a91a130
ElnurKasimov <ekasimov2012@gmail.com>
2023-10-24 21:25:30 +0300

    curved out the project from Hyperskill course

---
+++ src/Message.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class Message {
+    public static void printGreetings() {
+        System.out.println("Welcome to Amazing Numbers!");
+    }
+
+    public static void printInstructions() {
+        System.out.println("Supported requests:\n" +
+                "- enter a natural number to know its properties;\n" +
+                "- enter two natural numbers to obtain the properties of the list:\n" +
+                "  * the first parameter represents a starting number;\n" +
+                "  * the second parameter shows how many consecutive numbers are to be printed;\n" +
+                "- two natural numbers and properties to search for;\n" +
+                "- a property preceded by minus must not be present in numbers;\n" +
+                "- separate the parameters with one space;\n" +
+                "- enter 0 to exit.");
+    }
+
+    public static void printErrorFirstParameter() {
+        System.out.println("The first parameter should be a natural number or zero.");
+    }
+
+    public static void printErrorSecondParameter() {
+        System.out.println("The second parameter should be a natural number.");
+    }
+
+    public static void printErrorOnePropertyWrong(List<String> properties) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("The property [");
+        sb.append(properties.get(0));
+        for (int i = 1; i < properties.size(); i++) {
+            sb.append(", ").append(properties.get(i));
+        }
+        sb.append("] is wrong.");
+        System.out.println(sb.toString());
+        System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY, JUMPING, HAPPY, SAD]");
+    }
+
+    public static void printErrorManyPropertiesWrong(List<String> properties) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("The properties [");
+        sb.append(properties.get(0));
+        for (int i = 1; i < properties.size(); i++) {
+            sb.append(", ").append(properties.get(i));
+        }
+        sb.append("] are wrong.");
+        System.out.println(sb.toString());
+        System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY, JUMPING, HAPPY, SAD]");
+    }
+
+    public static void printErrorMutuallyExclusiveParameter(Map<String, String> map) {
+        System.out.print("The request contains mutually exclusive properties: ");
+        for(Map.Entry<String, String> pair : map.entrySet()) {
+            System.out.print("[" + pair.getKey() + ", " + pair.getValue() + "] ");
+        }
+        System.out.println();
+        System.out.println("There are no numbers with these properties.");
+    }
+}
---
+++ src/Service.java
@@ -0,0 +1,277 @@
+import java.util.*;
+
+public class Service {
+
+    public static void processAsOne (String[] parameters) {
+        long number = 0;
+        if (parameters[0].matches("\\d+")) {
+            number = Long.parseLong(parameters[0]);
+        } else {
+            Message.printErrorFirstParameter();
+            return;
+        }
+        if (number <= 0) {
+            Message.printErrorFirstParameter();
+        } else {
+            System.out.println("properties of " + number);
+            for (Map.Entry<String, String > entry : getProperties(number).entrySet()) {
+                System.out.println(entry.getKey() + " : " + entry.getValue());
+            }
+        }
+    }
+
+    public static void processAsTwo (String[] parameters) {
+        long firstNumber = 0;
+        long secondNumber = 0;
+        if (parameters[0].matches("\\d+")) {
+            firstNumber = Long.parseLong(parameters[0]);
+        } else {
+            Message.printErrorFirstParameter();
+            return;
+        }
+        if (parameters[1].matches("\\d+")) {
+            secondNumber = Long.parseLong(parameters[1]);
+        } else {
+            Message.printErrorSecondParameter();
+            return;
+        }
+
+        if (firstNumber <= 0) {
+            Message.printErrorFirstParameter();
+        } else if (secondNumber < 0) {
+            Message.printErrorSecondParameter();
+        } else {
+            for (long i = firstNumber; i < firstNumber + secondNumber; i++) {
+                System.out.print(i + " is ");
+                System.out.println(getPropertiesString(getProperties(i)));
+            }
+        }
+    }
+
+    public static void processAsMany (String[] parameters) {
+        long firstNumber = 0;
+        long secondNumber = 0;
+        if (parameters[0].matches("\\d+")) {
+            firstNumber = Long.parseLong(parameters[0]);
+        } else {
+            Message.printErrorFirstParameter();
+            return;
+        }
+        if (parameters[1].matches("\\d+")) {
+            secondNumber = Long.parseLong(parameters[1]);
+        } else {
+            Message.printErrorSecondParameter();
+            return;
+        }
+
+        if (firstNumber <= 0) {
+            Message.printErrorFirstParameter();
+        } else if (secondNumber < 0) {
+            Message.printErrorSecondParameter();
+        } else {
+            String[] lastParameters = Arrays.copyOfRange(parameters, 2, parameters.length);
+            List<String> properties = new ArrayList<>(Arrays.asList(lastParameters));
+            List<String> wrongProperties = checkProperty(properties);
+            switch (wrongProperties.size()) {
+                case 0 -> {}
+                case 1 -> {
+                    Message.printErrorOnePropertyWrong(wrongProperties);
+                    return;
+                }
+                default -> {
+                    Message.printErrorManyPropertiesWrong(wrongProperties);
+                    return;
+                }
+            }
+            Map<String, String> mutuallyExclusivePairs = checkPropertyMutuallyExclusive(properties);
+            if (!mutuallyExclusivePairs.isEmpty()) {
+                Message.printErrorMutuallyExclusiveParameter(mutuallyExclusivePairs);
+                return;
+            }
+            List<String> propertiesToAdd = new ArrayList<>();
+            List<String> propertiesToAvoid = new ArrayList<>();
+            for (String property : properties) {
+                if (property.charAt(0) == '-') {
+                    propertiesToAvoid.add(property.substring(1));
+                } else {
+                    propertiesToAdd.add(property);
+                }
+            }
+            int counter = 0;
+            while (counter < secondNumber) {
+                Map<String, String> numberProperties = getProperties(firstNumber);
+                if (propertiesToAvoid.isEmpty()) {
+                    if (containsSuchProperties(numberProperties, propertiesToAdd)) {
+                        counter++;
+                        System.out.print(firstNumber + " is ");
+                        System.out.println(getPropertiesString(getProperties(firstNumber)));
+                    }
+                } else {
+                    if (containsSuchProperties(numberProperties, propertiesToAdd)) {
+                        if (! containsSuchProperties(numberProperties, propertiesToAvoid)) {
+                            counter++;
+                            System.out.print(firstNumber + " is ");
+                            System.out.println(getPropertiesString(getProperties(firstNumber)));
+                        }
+                    }
+                }
+                firstNumber++;
+            }
+
+        }
+    }
+
+    private static List<String> checkProperty(List<String> properties) {
+        List<String> wrongProperties = new ArrayList<>();
+        List<String> correctProperties = new ArrayList<>(Arrays.asList(
+                "EVEN", "ODD", "BUZZ", "DUCK", "PALINDROMIC", "GAPFUL",
+                "SPY", "SQUARE", "SUNNY", "JUMPING", "HAPPY", "SAD",
+                "-EVEN", "-ODD", "-BUZZ", "-DUCK", "-PALINDROMIC", "-GAPFUL",
+                "-SPY", "-SQUARE", "-SUNNY", "-JUMPING", "-HAPPY", "-SAD"));
+        for (String property : properties) {
+            if (! correctProperties.contains(property.toUpperCase())) {
+                wrongProperties.add(property.toUpperCase());
+            }
+        }
+        return  wrongProperties;
+    }
+
+    private static Map<String, String> checkPropertyMutuallyExclusive(List<String> properties) {
+        properties.replaceAll(String::toUpperCase);
+        Map<String, String> result = new HashMap<>();
+        if (properties.contains("ODD") && properties.contains("EVEN")) {
+            result.put("ODD", "EVEN");
+        }
+        if (properties.contains("DUCK") && properties.contains("SPY")) {
+            result.put("DUCK", "SPY");
+        }
+        if (properties.contains("SUNNY") && properties.contains("SQUARE")) {
+            result.put("SUNNY", "SQUARE");
+        }
+        if (properties.contains("SAD") && properties.contains("HAPPY")) {
+            result.put("SAD", "HAPPY");
+        }
+        if (properties.contains("-ODD") && properties.contains("-EVEN")) {
+            result.put("-ODD", "-EVEN");
+        }
+        if (properties.contains("-DUCK") && properties.contains("-SPY")) {
+            result.put("-DUCK", "-SPY");
+        }
+        if (properties.contains("-SUNNY") && properties.contains("-SQUARE")) {
+            result.put("-SUNNY", "-SQUARE");
+        }
+        if (properties.contains("-SAD") && properties.contains("-HAPPY")) {
+            result.put("-SAD", "-HAPPY");
+        }
+
+        for (String property : properties) {
+            if (properties.contains(property) && properties.contains(property.substring(1))) {
+                result.put(property, property.substring(1));
+            }
+        }
+        return  result;
+    }
+
+    private static boolean containsSuchProperties(Map<String, String> numberProperties, List<String> inputProperties) {
+        boolean[] results = new boolean[inputProperties.size()];
+        for (int i = 0; i < inputProperties.size() ; i++) {
+            for (Map.Entry<String, String> entry : numberProperties.entrySet()) {
+                if (inputProperties.get(i).equalsIgnoreCase(entry.getKey()) && "true".equals(entry.getValue()) ) {
+                    results[i] = true;
+                    break;
+                }
+            }
+        }
+        boolean result = true;
+        for (boolean res : results) {
+            if (!res) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+
+    private static String getPropertiesString (Map<String, String> properties) {
+        StringBuilder propertyString = new StringBuilder();
+        for (Map.Entry<String, String> entry : properties.entrySet()) {
+            if ("true".equals(entry.getValue())) {
+                propertyString.append(entry.getKey()).append(", ");
+            }
+        }
+        propertyString.setLength(propertyString.length() - 2);
+        return propertyString.toString();
+    }
+
+    public static List<Long> getFiguresFromNumber(long number) {
+        List<Long> figures = new ArrayList<>();
+        while (number != 0) {
+            figures.add(number % 10);
+            number /= 10;
+        }
+        Collections.reverse(figures);
+        return figures;
+    }
+
+    private static Map<String, String> getProperties (long number) {
+        Map<String, String> propertyMap = new HashMap<>();
+        NumberProperties property = new NumberProperties(number);
+        if (property.isEven()) {
+            propertyMap.put("even", "true");
+            propertyMap.put("odd", "false");
+        } else {
+            propertyMap.put("even", "false");
+            propertyMap.put("odd", "true");
+        }
+        if (property.isBuzz()) {
+            propertyMap.put("buzz", "true");
+        } else {
+            propertyMap.put("buzz", "false");
+        }
+        if (property.isDuck()) {
+            propertyMap.put("duck", "true");
+        } else {
+            propertyMap.put("duck", "false");
+        }
+        if (property.isPalindromic()) {
+            propertyMap.put("palindromic", "true");
+        } else {
+            propertyMap.put("palindromic", "false");
+        }
+        if (property.isGapful()) {
+            propertyMap.put("gapful", "true");
+        } else {
+            propertyMap.put("gapful", "false");
+        }
+        if (property.isSpy()) {
+            propertyMap.put("spy", "true");
+        } else {
+            propertyMap.put("spy", "false");
+        }
+        if (property.isSquare()) {
+            propertyMap.put("square", "true");
+        } else {
+            propertyMap.put("square", "false");
+        }
+        if (property.isSunny()) {
+            propertyMap.put("sunny", "true");
+        } else {
+            propertyMap.put("sunny", "false");
+        }
+        if (property.isJumping()) {
+            propertyMap.put("jumping", "true");
+        } else {
+            propertyMap.put("jumping", "false");
+        }
+        if (property.isHappy()) {
+            propertyMap.put("happy", "true");
+            propertyMap.put("sad", "false");
+        } else {
+            propertyMap.put("happy", "false");
+            propertyMap.put("sad", "true");
+        }
+
+        return propertyMap;
+    }
+}
